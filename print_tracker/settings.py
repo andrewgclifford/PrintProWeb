e58cfb4345df3e505390719fd2b97b6af30e715b
Andrew Clifford <agclifford01@gmail.com>
2024-12-06 12:38:09 -0500

    Add TEMPLATES configuration for Django admin

--- print_tracker/settings.py
+++ print_tracker/settings.py
@@ -58,6 +58,23 @@ REST_FRAMEWORK = {
     ],
 }
 
+# Templates configuration
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
