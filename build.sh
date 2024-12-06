cf77b34a9efa5639757b27b6dc56ba3150be7cb0
Andrew Clifford <agclifford01@gmail.com>
2024-12-06 13:13:33 -0500

    Update build script to create superuser

--- build.sh
+++ build.sh
@@ -17,3 +17,14 @@ python manage.py migrate sessions
 python manage.py migrate accounts
 python manage.py migrate jobs
 python manage.py migrate
+
+# Create superuser
+python manage.py shell << EOF
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username='$DJANGO_SUPERUSER_USERNAME').exists():
+    User.objects.create_superuser('$DJANGO_SUPERUSER_USERNAME', 
+                                '$DJANGO_SUPERUSER_EMAIL',
+                                '$DJANGO_SUPERUSER_PASSWORD',
+                                user_type='admin')
+EOF
