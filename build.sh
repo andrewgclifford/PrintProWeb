53514242e55247f108b02bc12e696451713eac38
Andrew Clifford <agclifford01@gmail.com>
2024-12-06 13:04:26 -0500

    Add initial migration for accounts app

--- build.sh
+++ build.sh
@@ -5,6 +5,15 @@ set -o errexit
 # Install python dependencies
 pip install -r requirements.txt
 
+# Create migrations for each app
+python manage.py makemigrations accounts
+python manage.py makemigrations jobs
+
 # Run migrations
-python manage.py makemigrations
+python manage.py migrate auth
+python manage.py migrate contenttypes
+python manage.py migrate admin
+python manage.py migrate sessions
+python manage.py migrate accounts
+python manage.py migrate jobs
 python manage.py migrate
