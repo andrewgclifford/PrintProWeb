d35602889130098e8caa6c0d62a1e58941c93ac1
Andrew Clifford <agclifford01@gmail.com>
2024-12-06 13:34:12 -0500

    Implement custom MultipleFileField for proper file upload handling

--- jobs/forms.py
+++ jobs/forms.py
@@ -1,10 +1,26 @@
 from django import forms
 from .models import Job, JobFile, Comment
 
+class MultipleFileInput(forms.ClearableFileInput):
+    allow_multiple_selected = True
+
+class MultipleFileField(forms.FileField):
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault("widget", MultipleFileInput())
+        super().__init__(*args, **kwargs)
+
+    def clean(self, data, initial=None):
+        single_file_clean = super().clean
+        if isinstance(data, (list, tuple)):
+            result = [single_file_clean(d, initial) for d in data]
+        else:
+            result = single_file_clean(data, initial)
+        return result
+
 class JobForm(forms.ModelForm):
-    files = forms.FileField(
-        widget=forms.FileInput(attrs={'multiple': True, 'class': 'form-control'}),
-        required=False
+    files = MultipleFileField(
+        required=False,
+        widget=MultipleFileInput(attrs={'class': 'form-control', 'multiple': True})
     )
     
     class Meta:
@@ -69,6 +85,21 @@ class JobForm(forms.ModelForm):
         # Format initial datetime value for the deadline field
         if self.instance.pk and self.instance.deadline:
             self.initial['deadline'] = self.instance.deadline.strftime('%Y-%m-%dT%H:%M')
+
+    def save(self, commit=True):
+        instance = super().save(commit=commit)
+        
+        # Handle multiple file uploads
+        files = self.cleaned_data.get('files')
+        if files:
+            for file in files:
+                JobFile.objects.create(
+                    job=instance,
+                    file=file,
+                    file_type=file.content_type
+                )
+        
+        return instance
 
 class CommentForm(forms.ModelForm):
     class Meta:
